cb5a2b0e0d985332cb4e2d7fcdf05285ad3b07b2
chileitsr3nzy <renishnkirote@gmail.com>
2024-02-21 09:26:34 +0300

    hire.purchase .py

---
+++ week_1/hire.purchase .py
@@ -0,0 +1,11 @@
+#program that calculates hire purchase price
+#Date : 20/02/2024
+#Name : Renish Nkirote
+dep = float(input("enter the deposited amount :" ))
+n =float(input("enter the number of months : "))
+inst =float(input("enter the monthly instalment paid "))
+
+hpp = dep + ( n + inst )
+
+print("There higher purchase price is : ", hpp)
+
