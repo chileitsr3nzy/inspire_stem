3f7b4c232ba7e3fdbb591763976f3b3465f42606
chileitsr3nzy <renishnkirote@gmail.com>
2024-02-24 19:53:05 +0300

    palindrome.py ,pascals triange.py

---
+++ week_2/m_table.py
@@ -0,0 +1,35 @@
+#m table
+#Date : 22/02/2024
+#Name : Renish Nkirote
+
+for i in range (1,11):
+
+    a=i*1
+    b=i*2
+    c=i*3
+    d=i*4
+    e=i*5
+    f=i*6
+    g=i*7
+    h=i*8
+    i=i*9
+    j=i*10
+    
+
+
+    print(str(a) + "\t" ,end=" ")
+    print(str(b) + "\t" ,end=" ")
+    print(str(c) + "\t" ,end=" ")
+    print(str(d) + "\t" ,end=" ")
+    print(str(e) + "\t" ,end=" ")
+    print(str(f) + "\t" ,end=" ")
+    print(str(g) + "\t" ,end=" ")
+    print(str(h) + "\t" ,end=" ")
+    print(str(i) + "\t" ,end=" ")
+    print(str(j) + "\t" ,end=" ")
+
+#printing multiplication table 
+    for i in range(1,11):
+        for j in range(1,11):
+            print(i,"x"j,"=",i*j,end="\t")
+    
---
+++ week_2/palindrome.py
@@ -0,0 +1,27 @@
+#to show if a word is a palindrome
+#Date : 22/02/2024
+#Name : Renish Nkirote
+
+name ="reinier"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+
+
+
+print("\t")
+
+#reverse
+print(name[0])
+print(name[-6])
+print(name[-5])
+print(name[-4])
+print(name[-3])
+print(name[-2])
+print(name[-1])
